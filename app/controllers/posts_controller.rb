39cccdcffe2951559f3314b15325244b37c3a92b
Tera0592 <69949838+Tera0592@users.noreply.github.com>
2020-09-07 09:22:15 +0900

    メモ投稿機能のフロントサイドの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,8 @@ class PostsController < ApplicationController
   # def new
   # end
   def create
-    post = Post.create(content: params[:content],checked: false)
-    render = json:{ post: post }
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
 
   def checked
